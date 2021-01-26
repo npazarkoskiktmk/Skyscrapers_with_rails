82b6a6bba47c6eb8a6435393874c54f77ff21c9b
npazarkoskiktmk <nikola@kennedytech.mk>
2021-01-26 10:38:25 +0100

    post route for sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'pages/index'
   root 'pages#index'
   get 'pages/signin'
+  post 'pages/signin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
