056287e64240007f64ac7f7b2332806680e1a792
npazarkoskiktmk <nikola@kennedytech.mk>
2021-02-04 11:15:18 +0100

    Seeds file change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |item|
+
+    Book.create!(
+        title: "Title nr. #{item}",
+        description: "Description nr: #{item}",
+        author: "Author #{item}",
+        year: "202#{item}".to_i
+    )
+
+    user = User.new
+    user.username= "User nr.: #{item}"
+    user.first_name= "First name #{item}"
+    user.last_name= "Last name #{item}"
+    user.email = "user#{item}@test.com"
+    user.password = "password#{item}"
+    user.password_confirmation = "password#{item}"
+    user.save!
+
+    Feedback.create!(
+        comment: "Comment #{item}",
+        book_id: item+1,
+        user_id: item+1
+    )
+
+end
