073c6c1b96b142fed5cc930ff2c38e0f72bbad7b
npazarkoskiktmk <nikola@kennedytech.mk>
2021-02-04 12:15:45 +0100

    Faker gem implementation

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '~> 1.4'
+  # Faker gem
+  gem 'faker', git: 'https://github.com/faker-ruby/faker.git', branch: 'master'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,41 @@
-10.times do |item|
+ require 'faker'
 
+ puts "Starting seeds..."
+
+    User.create!(
+        username: "testuser",
+        first_name: "User",
+        last_name: "User",
+        email: "user@test.com",
+        password: "password",
+        password_confirmation: "password"
+    )
+
+puts "Creating books and users..."
+10.times do |item|
     Book.create!(
-        title: "Title nr. #{item}",
-        description: "Description nr: #{item}",
-        author: "Author #{item}",
+        title: Faker::Book.title,
+        description: Faker::Book.genre,
+        author: Faker::Book.author,
         year: "202#{item}".to_i
     )
-
-    user = User.new
-    user.username= "User nr.: #{item}"
-    user.first_name= "First name #{item}"
-    user.last_name= "Last name #{item}"
-    user.email = "user#{item}@test.com"
-    user.password = "password#{item}"
-    user.password_confirmation = "password#{item}"
-    user.save!
-
-    Feedback.create!(
-        comment: "Comment #{item}",
-        book_id: item+1,
-        user_id: item+1
+    User.create!(
+        username: Faker::Internet.username(specifier: 5..8),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name ,
+        email: Faker::Internet.email,
+        password: "password",
+        password_confirmation: "password"
     )
-
 end
+
+puts "Creating feedback..."
+30.times do
+    Feedback.create!(
+        comment: Faker::Lorem.sentence(word_count: 6),
+        book_id: Faker::Number.within(range: 1..10),
+        user_id: Faker::Number.within(range: 1..11)
+    )
+end
+
+puts "Seeding completed!"
